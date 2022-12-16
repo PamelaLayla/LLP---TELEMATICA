cd67dd5d21bc07ce93fb54e94f81599cc5bb968d
Pâmela Layla Freitas Barbosa <84047189+PamelaLayla@users.noreply.github.com>
2022-12-16 19:54:22 -0300

    AV5
    Avaliação05

---
+++ AV5.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define TAMANHO 100
+
+int main()
+{
+char texto[TAMANHO];
+FILE* fp;
+
+  printf("Digite o texto que deseja gravar num arquivo.\n");
+fgets(texto, TAMANHO, stdin);
+if((fp = fopen("arquivo.txt", "w"))==NULL)
+{
+  printf("O arquivo não pode ser aberto.\n");
+exit(1);
+}
+fputs(texto, fp);
+fclose(fp);
+
+
+return 0;
+}
